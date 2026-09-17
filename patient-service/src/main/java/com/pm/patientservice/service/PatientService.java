9f3e795e468705ad7e79699241a60eabffef8444
Karan-Mansuria <karanmansuria35@gmail.com>
2026-09-17 23:17:13 +0530

    Small Changes

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,11 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PatientService {
+
+
+}
